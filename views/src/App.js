33522d41b628f984f798e8fb619b783d71ffa26d
User <chiruzabisimwa@gmail.com>
2021-08-03 16:12:35 +0200

    Props in class extends

--- views/src/App.js
+++ views/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Events from './components/Events'
+import Props from './components/Props'
 
 class App extends React.Component {
 
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div>
         <h3>React VsLearn.IO</h3>
         <hr />
+        <Props />
         <Events />
       </div>
     )
---
+++ views/src/components/Props.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Props extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Props part</h3>
+            </div>
+        )
+    }
+}
+export default Props
