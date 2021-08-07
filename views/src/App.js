352c9d5d85cb46ea82ea8688a013fc745fedf601
User <chiruzabisimwa@gmail.com>
2021-08-07 12:48:00 +0200

    My changes

--- views/src/App.js
+++ views/src/App.js
@@ -14,13 +14,23 @@ class App extends React.Component {
       <div>
         <div className="box-header">
           <h3>React VsLearn.IO</h3>
+          <div className="form">
+            <h2>Data from an API</h2>
+          </div>
         </div>
         <Challenge />
         <ChallengeTwo />
-        <hr />
         <Props />
         <Events />
         <Form />
+        <div className="container">
+          <div className="form">
+            <h3>Data </h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut provident dignissimos facilis tenetur nostrum aliquid, dolor eos cum est? Iste sint ad necessitatibus veritatis excepturi magnam voluptate, sequi, voluptas ut reiciendis pariatur eius voluptatibus minima natus explicabo facilis nulla commodi repellat repudiandae nam obcaecati ex neque nemo quasi. Consequatur, numquam.
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
